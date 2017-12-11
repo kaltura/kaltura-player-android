eef5ce24b6587d4644ad3c0b68db6148f39e30c4
Noam Tamim <noam.tamim@kaltura.com>
2017-12-11 23:22:30 +0200

    Clean up the drawer

--- ovpplayerdemo/src/main/java/com/kaltura/ovpplayerdemo/MainActivity.java
+++ ovpplayerdemo/src/main/java/com/kaltura/ovpplayerdemo/MainActivity.java
@@ -171,17 +171,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_player) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_downloads) {
 
         }
 
