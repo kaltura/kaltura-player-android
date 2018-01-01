c5360447859ee36b497b36db3d23d1f292406caa
Noam Tamim <noam.tamim@kaltura.com>
2018-01-01 14:59:20 +0200

    Known plugins enum

--- baseplayer/src/main/java/com/kaltura/tvplayer/KalturaPlayer.java
+++ baseplayer/src/main/java/com/kaltura/tvplayer/KalturaPlayer.java
@@ -37,6 +37,83 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+// The 
+enum KnownPlugin {
+    
+    // Kaltura OVP Analytics
+    kava("com.kaltura.playkit.plugins.kava.KavaAnalyticsPlugin", KavaAnalyticsPlugin.factory),
+    stats("com.kaltura.playkit.plugins.ovp.KalturaStatsPlugin", KalturaStatsPlugin.factory),
+    liveStats("com.kaltura.playkit.plugins.ovp.KalturaLiveStatsPlugin", KalturaLiveStatsPlugin.factory),
+    
+    // Kaltura Phoenix MediaHit/MediaMark
+    phoenixBookmarks("com.kaltura.playkit.plugins.ott.PhoenixAnalyticsPlugin"),
+    
+    // IMA
+    ima("com.kaltura.playkit.plugins.ima.IMAPlugin"),
+    
+    // Youbora
+    youbora("com.kaltura.playkit.plugins.youbora.YouboraPlugin"),
+    ;
+
+
+    private static final PKLog log = PKLog.get("KnownPlugin");
+
+    public final String className;
+    public final PKPlugin.Factory factory;
+
+    KnownPlugin(String className, PKPlugin.Factory factory) {
+        this.className = className;
+        this.factory = factory;
+    }
+
+    KnownPlugin(String className) {
+        this(className, null);
+    }
+
+    protected static void registerPluginByName(Context context, String pluginClassName) {
+        try {
+            Class pluginClass = Class.forName(pluginClassName);
+            final Field factoryField = pluginClass.getField("factory");
+            if (!Modifier.isStatic(factoryField.getModifiers())) {
+                log.e("Plugin factory " + pluginClassName + ".factory is not static");
+                return;
+            }
+            final PKPlugin.Factory factory = (PKPlugin.Factory) factoryField.get(null);
+            if (factory == null) {
+                log.e("Plugin factory " + pluginClassName + ".factory is null");
+                return;
+            }
+            PlayKitManager.registerPlugins(context, factory);
+            
+        } catch (ClassNotFoundException e) {
+            // This is ok and very common
+            log.v("Plugin class " + pluginClassName + " not found");
+        } catch (NoSuchFieldException e) {
+            log.e("Plugin factory " + pluginClassName + ".factory not found");
+        } catch (IllegalAccessException e) {
+            log.e("Plugin factory " + pluginClassName + ".factory is not public");
+        } catch (ClassCastException e) {
+            log.e("Plugin factory " + pluginClassName + ".factory is not a PKPlugin.Factory");
+        } catch (RuntimeException e) {
+            log.e("Something bad", e);
+        }
+    }
+
+    void register(Context context) {
+        if (factory != null) {
+            PlayKitManager.registerPlugins(context, factory);
+        } else {
+            registerPluginByName(context, className);
+        }
+    }
+    
+    static void registerAll(Context context) {
+        for (KnownPlugin plugin : values()) {
+            plugin.register(context);
+        }
+    }
+}
+
 public abstract class KalturaPlayer <MOT extends MediaOptions> {
 
     private static final PKLog log = PKLog.get("KalturaPlayer");
@@ -188,48 +265,11 @@ public abstract class KalturaPlayer <MOT extends MediaOptions> {
         
         setMedia(entry);
     }
-    
-    protected void registerPluginByName(Context context, String pluginClassName) {
-        try {
-            Class pluginClass = Class.forName(pluginClassName);
-            final Field factoryField = pluginClass.getField("factory");
-            if (!Modifier.isStatic(factoryField.getModifiers())) {
-                log.e("Plugin factory " + pluginClassName + ".factory is not static");
-                return;
-            }
-            final PKPlugin.Factory factory = (PKPlugin.Factory) factoryField.get(null);
-            if (factory == null) {
-                log.e("Plugin factory " + pluginClassName + ".factory is null");
-                return;
-            }
-            PlayKitManager.registerPlugins(context, factory);
-            
-        } catch (ClassNotFoundException e) {
-            // This is ok and very common
-            log.v("Plugin class " + pluginClassName + " not found");
-        } catch (NoSuchFieldException e) {
-            log.e("Plugin factory " + pluginClassName + ".factory not found");
-        } catch (IllegalAccessException e) {
-            log.e("Plugin factory " + pluginClassName + ".factory is not public");
-        } catch (ClassCastException e) {
-            log.e("Plugin factory " + pluginClassName + ".factory is not a PKPlugin.Factory");
-        } catch (RuntimeException e) {
-            log.e("Something bad", e);
-        }
-    }
 
     protected void registerCommonPlugins(Context context) {
-        PlayKitManager.registerPlugins(context,
-                KavaAnalyticsPlugin.factory,
-                KalturaStatsPlugin.factory,
-                KalturaLiveStatsPlugin.factory
-        );
-
-        registerPluginByName(context, "com.kaltura.playkit.plugins.ima.IMAPlugin");
-        registerPluginByName(context, "com.kaltura.playkit.plugins.youbora.YouboraPlugin");
+        KnownPlugin.registerAll(context);
     }
-
-
+    
 
     public void setKS(String ks) {
         this.ks = ks;
