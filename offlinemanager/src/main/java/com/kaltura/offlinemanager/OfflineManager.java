1081e0c4f2073ce4d89b8a3ac15a345a33c25da6
Noam Tamim <noam.tamim@kaltura.com>
2018-02-19 13:14:28 +0200

    Structural changes to Offline Manager classes.

--- offlinemanager/src/main/java/com/kaltura/offlinemanager/OfflineManager.java
+++ offlinemanager/src/main/java/com/kaltura/offlinemanager/OfflineManager.java
@@ -1,14 +1,12 @@
 package com.kaltura.offlinemanager;
 
-import android.support.annotation.NonNull;
-
+import com.kaltura.dtg.DownloadState;
 import com.kaltura.playkit.PKMediaEntry;
 import com.kaltura.tvplayer.MediaOptions;
 
-import java.io.IOException;
 import java.util.List;
 
-
+@SuppressWarnings({"WeakerAccess", "unused", "JavaDoc"})
 public abstract class OfflineManager {
 
     public static OfflineManager getInstance() {
@@ -19,7 +17,7 @@ public abstract class OfflineManager {
      * Set the global download state listener, to be notified about state changes.
      * @param listener
      */
-    public abstract void setDownloadStateListener(DownloadStateListener listener);
+    public abstract void setAssetInfoUpdateListener(AssetInfoUpdateListener listener);
 
     /**
      * Set the global download progress listener
@@ -27,141 +25,182 @@ public abstract class OfflineManager {
      */
     public abstract void setDownloadProgressListener(DownloadProgressListener listener);
 
+    /**
+     * Set the global DRM license update listener.
+     * @param listener
+     * @see DrmLicenseUpdateListener
+     */
+    public abstract void setDrmLicenseUpdateListener(DrmLicenseUpdateListener listener);
+
+    /**
+     * Temporarily pause all downloads. Doesn't change assets' download state. Revert with {@link #resumeDownloads()}.
+     */
     public abstract void pauseDownloads();
 
+    /**
+     * Opposite of {@link #pauseDownloads()}.
+     */
     public abstract void resumeDownloads();
 
-
-
+    /**
+     * Find asset by id.
+     * @param assetId
+     * @return asset info or null if not found.
+     */
+    public abstract AssetInfo getAssetInfo(String assetId);
 
     /**
-     * Find entry by id. Returns null if the entry is not found.
-     * @param entryId
+     * Get list of {@link AssetInfo} objects for all assets in the given state.
+     * @param state
      * @return
      */
-    public abstract EntryMetadata getEntryMetadata(String entryId);
+    public abstract List<AssetInfo> getAssetsByState(DownloadState state);
 
     /**
-     * Get an entry's PKMediaEntry object, as stored by {@link #addEntry(PKMediaEntry)} or
-     * {@link #addEntry(int, String, String, MediaOptions)}.
-     * @param entryId
+     * Get an asset's PKMediaEntry object, as stored by {@link #addAsset(PKMediaEntry)} or
+     * {@link #addAsset(int, String, String, MediaOptions)}.
+     * @param assetId
      * @return
      */
-    public abstract PKMediaEntry getMediaEntry(String entryId);
+    public abstract PKMediaEntry getOriginalMediaEntry(String assetId);
 
     /**
      * Get an offline-playable PKMediaEntry object.
-     * @param entryId
+     * @param assetId
      * @return
      */
-    public abstract PKMediaEntry getLocalEntry(String entryId);
+    public abstract PKMediaEntry getLocalPlaybackEntry(String assetId);
 
     /**
-     * Add a new entry with information from mediaEntry. All relevant information from mediaEntry is
+     * Add a new asset with information from mediaEntry. All relevant metadata from mediaEntry is
      * stored, but only for the selection download source.
      * @param mediaEntry
-     * @return
+     * @return true if the asset was added, false otherwise. Note: returns false if asset already exists.
      */
-    public abstract EntryMetadata addEntry(PKMediaEntry mediaEntry);
+    public abstract boolean addAsset(PKMediaEntry mediaEntry);
 
     /**
-     * Add a new entry by connecting to the backend with the provided details.
+     * Add a new asset by connecting to the backend with the provided details.
      * @param partnerId
      * @param ks
      * @param serverUrl
      * @param mediaOptions
-     * @return a reference to the new entry or null if the entry already exists.
+     * @return true if the asset was added, false otherwise. Note: returns false if asset already exists.
      */
-    public abstract EntryMetadata addEntry(int partnerId, String ks, String serverUrl, MediaOptions mediaOptions);
+    public abstract boolean addAsset(int partnerId, String ks, String serverUrl, MediaOptions mediaOptions);
 
     /**
-     * Load entry's metadata (tracks, size, etc).
-     * @param entryId
-     * @param listener
-     * @return false if entry is not found.
+     * Load asset's metadata (tracks, size, etc).
+     * @param assetId
+     * @param trackSelectionListener
+     * @param assetInfoUpdateListener
+     * @return false if asset is not found.
      */
-    public abstract boolean loadMetadata(String entryId, MediaSelection selection, MetadataListener listener);
+    public abstract boolean loadAssetDownloadInfo(String assetId, MediaPrefs selection,
+                                                  TrackSelectionListener trackSelectionListener,
+                                                  AssetInfoUpdateListener assetInfoUpdateListener);
 
 
     /**
-     * Start (or resume) downloading an entry.
-     * @param entryId
-     * @return false if entry is not found.
+     * Start (or resume) downloading an asset.
+     * @param assetId
+     * @return false if asset is not found, true otherwise.
      */
-    public abstract boolean startDownload(String entryId);
+    public abstract boolean startDownload(String assetId);
 
     /**
-     * Pause downloading an entry.
-     * @param entryId
-     * @return false if entry is not found.
+     * Pause downloading an asset.
+     * @param assetId
+     * @return false if asset is not found, true otherwise.
      */
-    abstract boolean pauseDownload(String entryId);
+    public abstract boolean pauseDownload(String assetId);
 
     /**
-     * Remove entry with all its data.
-     * @param entryId
-     * @return false if entry is not found.
+     * Remove asset with all its data.
+     * @param assetId
+     * @return false if asset is not found, true otherwise.
      */
-    abstract boolean removeEntry(String entryId);
+    public abstract boolean removeEntry(String assetId);
 
     /**
-     * Check the license status of an entry.
-     * @param entryId
-     * @return false if entry is not found.
+     * Check the license status of an asset.
+     * @param assetId
+     * @return DRM license status - {@link AssetDrmInfo}.
      */
-    abstract boolean checkDrmLicense(String entryId);
+    public abstract AssetDrmInfo getDrmStatus(String assetId);
 
     /**
-     * Renew an entry's license.
-     * @param entryId
-     * @return false if entry is not found.
+     * Renew an asset's license. The result is passed asynchronously to the global {@link DrmLicenseUpdateListener}.
+     * @param assetId
+     * @return false if asset is not found, true otherwise.
      */
-    abstract boolean renewDrmLicense(String entryId);
+    public abstract boolean renewAssetDrmLicense(String assetId);
 
-    public interface MetadataListener {
-        void onTracksAvailable(String entryId, TrackSelector trackSelector);
-        void onMetadataReady(String entryId, EntryMetadata entryMetadata);
+    /**
+     * Invoked during asset info loading ({@link #loadAssetDownloadInfo(String, MediaPrefs, TrackSelectionListener, AssetInfoUpdateListener)}).
+     * Allows the app to inspect and change the track selection.
+     * @see {@link MediaPrefs} for higher-level track selection customization.
+     */
+    public interface TrackSelectionListener {
+        void onTracksAvailable(String assetId, TrackSelector trackSelector);
     }
 
+    /**
+     * Invoked while downloading an asset; use with {@link #setDownloadProgressListener(DownloadProgressListener)}.
+     */
     public interface DownloadProgressListener {
-        void onDownloadProgress(String entryId, long downloadedBytes, long totalEstimatedBytes);
+        void onDownloadProgress(String assetId, long downloadedBytes, long totalEstimatedBytes);
     }
 
-    public interface DownloadStateListener {
-        void onDownloadStateChange(String entryId, EntryMetadata metadata, EntryDownloadState downloadState);
+    /**
+     * Invoked during asset info loading ({@link #loadAssetDownloadInfo(String, MediaPrefs, TrackSelectionListener, AssetInfoUpdateListener)}).
+     */
+    public interface AssetInfoUpdateListener {
+        void onAssetInfoUpdated(AssetInfo assetInfo);
     }
 
-    public interface DRMListener {
-        void onLicenceInstalled(String entryId);
-        void onLicenseRemoved(String entryId);
-        void onLicenseRenewed(String entryId);
+    /**
+     * Global listener for DRM actions. Use with {@link #setDrmLicenseUpdateListener(DrmLicenseUpdateListener)}.
+     */
+    public interface DrmLicenseUpdateListener {
+        void onLicenceInstalled(String assetId, int totalTime, int timeToRenew);
+        void onLicenseRenewed(String assetId, int totalTime, int timeToRenew);
+        void onLicenseRemoved(String assetId);
+    }
+
+    public static class AssetDrmInfo {
+        public Status status;
+        public int totalRemainingTime;
+        public int currentRemainingTime;
+
+        public enum Status {
+            valid, unknown, expired, clear
+        }
     }
 
     public interface TrackSelector {
-        List<Track> getAvailableTracks(@NonNull TrackType type);
-        List<Track> getDownloadedTracks(@NonNull TrackType type);
-        void setSelectedTracks(@NonNull TrackType type, @NonNull List<Track> tracks);
-        void apply() throws IOException;
+        List<Track> getAvailableTracks(TrackType type);
+        List<Track> getSelectedTracks(TrackType type);
+        void setSelectedTracks(TrackType type, List<Track> tracks);
     }
 
-    public interface Track {
-        TrackType getType();
-        String getLanguage();
-        long getBitrate();
-        CodecType getCodec();
-        int getWidth();
-        int getHeight();
+    public static class Track {
+        TrackType type;
+        String language;
+        CodecType codec;
+        long bitrate;
+        int width;
+        int height;
     }
 
-    public interface EntryMetadata {
-        String id();
-        String url();
-        Long estimatedSize();
-        Long downloadedSize();
-        EntryDownloadState state();
+    public static class AssetInfo {
+        String id;
+        AssetDownloadState state;
+        long estimatedSize;
+        long downloadedSize;
     }
 
-    public enum EntryDownloadState {
+    public enum AssetDownloadState {
         added, metadataLoaded, started, paused, completed, failed
     }
 
@@ -173,7 +212,10 @@ public abstract class OfflineManager {
         avc, hevc, vp9
     }
 
-    public static class MediaSelection {
+    /**
+     * Pre-download media preferences. Used with {@link #loadAssetDownloadInfo(String, MediaPrefs, TrackSelectionListener, AssetInfoUpdateListener)}.
+     */
+    public static class MediaPrefs {
         public Long preferredVideoBitrate;
         public Long preferredVideoHeight;
         public Long preferredVideoWidth;
@@ -181,27 +223,27 @@ public abstract class OfflineManager {
         public List<String> preferredAudioLanguages;
         public List<String> preferredTextLanguages;
 
-        public MediaSelection setPreferredVideoBitrate(Long preferredVideoBitrate) {
+        public MediaPrefs setPreferredVideoBitrate(Long preferredVideoBitrate) {
             this.preferredVideoBitrate = preferredVideoBitrate;
             return this;
         }
 
-        public MediaSelection setPreferredVideoHeight(Long preferredVideoHeight) {
+        public MediaPrefs setPreferredVideoHeight(Long preferredVideoHeight) {
             this.preferredVideoHeight = preferredVideoHeight;
             return this;
         }
 
-        public MediaSelection setPreferredVideoWidth(Long preferredVideoWidth) {
+        public MediaPrefs setPreferredVideoWidth(Long preferredVideoWidth) {
             this.preferredVideoWidth = preferredVideoWidth;
             return this;
         }
 
-        public MediaSelection setPreferredAudioLanguages(List<String> preferredAudioLanguages) {
+        public MediaPrefs setPreferredAudioLanguages(List<String> preferredAudioLanguages) {
             this.preferredAudioLanguages = preferredAudioLanguages;
             return this;
         }
 
-        public MediaSelection setPreferredTextLanguages(List<String> preferredTextLanguages) {
+        public MediaPrefs setPreferredTextLanguages(List<String> preferredTextLanguages) {
             this.preferredTextLanguages = preferredTextLanguages;
             return this;
         }
--- offlinemanager/src/main/java/com/kaltura/offlinemanager/OfflineManagerImp.java
+++ offlinemanager/src/main/java/com/kaltura/offlinemanager/OfflineManagerImp.java
@@ -1,27 +1,36 @@
 package com.kaltura.offlinemanager;
 
 import com.kaltura.dtg.ContentManager;
+import com.kaltura.dtg.DownloadState;
 import com.kaltura.playkit.LocalAssetsManager;
 import com.kaltura.playkit.PKMediaEntry;
 import com.kaltura.tvplayer.MediaOptions;
 
+import java.util.List;
+
 // PlayKit entryId === DTG itemId
 
 class OfflineManagerImp extends OfflineManager {
-    private DownloadStateListener downloadStateListener;
+    private AssetInfoUpdateListener assetInfoUpdateListener;
     private DownloadProgressListener downloadProgressListener;
 
     private LocalAssetsManager localAssetsManager;
     private ContentManager contentManager;
+    private DrmLicenseUpdateListener drmLicenseUpdateListener;
 
     @Override
-    public void setDownloadStateListener(DownloadStateListener listener) {
-        this.downloadStateListener = listener;
+    public void setAssetInfoUpdateListener(AssetInfoUpdateListener listener) {
+        this.assetInfoUpdateListener = listener;
     }
 
     @Override
     public void setDownloadProgressListener(DownloadProgressListener listener) {
         this.downloadProgressListener = listener;
+    }
+
+    @Override
+    public void setDrmLicenseUpdateListener(DrmLicenseUpdateListener listener) {
+        drmLicenseUpdateListener = listener;
     }
 
     @Override
@@ -35,71 +44,76 @@ class OfflineManagerImp extends OfflineManager {
     }
 
     @Override
-    public EntryMetadata getEntryMetadata(String entryId) {
-        // TODO: 28/01/2018 DTG lookup
+    public AssetInfo getAssetInfo(String assetId) {
         return null;
     }
 
     @Override
-    public PKMediaEntry getMediaEntry(String entryId) {
+    public List<AssetInfo> getAssetsByState(DownloadState state) {
+        // TODO: 15/02/2018 DTG get downloads
+        return null;
+    }
+
+    @Override
+    public PKMediaEntry getOriginalMediaEntry(String assetId) {
         // TODO: 28/01/2018 DTG lookup, get entry json from app data
         return null;
     }
 
     @Override
-    public PKMediaEntry getLocalEntry(String entryId) {
+    public PKMediaEntry getLocalPlaybackEntry(String assetId) {
         // TODO: 28/01/2018 DTG local file and LocalAssetManager local source
         return null;
     }
 
     @Override
-    public EntryMetadata addEntry(PKMediaEntry mediaEntry) {
+    public boolean addAsset(PKMediaEntry mediaEntry) {
         // TODO: 28/01/2018 Add DTG item and save mediaEntry as json
-        return null;
-    }
-
-    @Override
-    public EntryMetadata addEntry(int partnerId, String ks, String serverUrl, MediaOptions mediaOptions) {
-        // TODO: 28/01/2018 Use media provider, addEntry()
-        return null;
-    }
-
-    @Override
-    public boolean loadMetadata(String entryId, MediaSelection selection, MetadataListener listener) {
-        // TODO: 29/01/2018 Use DTG's loadMetadata. Apply MediaSelection in onTracksAvailable. Call listener.
-        // The default behavior for video selection is to choose the best track for the device:
-        // 1. Dimensions: should match screen size
-        // 2. DRM requirements
-        // 3. MediaSelection parameters
         return false;
     }
 
     @Override
-    public boolean startDownload(String entryId) {
+    public boolean addAsset(int partnerId, String ks, String serverUrl, MediaOptions mediaOptions) {
+        // TODO: 28/01/2018 Use media provider, addAsset()
+        return false;
+    }
+
+    @Override
+    public boolean loadAssetDownloadInfo(String assetId, MediaPrefs selection, TrackSelectionListener trackSelectionListener, AssetInfoUpdateListener assetInfoUpdateListener) {
+        // TODO: 29/01/2018 Use DTG's loadMetadata. Apply MediaPrefs in onTracksAvailable. Call listener.
+        // The default behavior for video selection is to choose the best track for the device:
+        // 1. Dimensions: should match screen size
+        // 2. DRM requirements
+        // 3. MediaPrefs parameters
+        return false;
+    }
+
+    @Override
+    public boolean startDownload(String assetId) {
         // TODO: 29/01/2018 DTG start item. IF ANOTHER ITEM IS IN PROGRESS, DON'T START yet.
         return false;
     }
 
     @Override
-    public boolean pauseDownload(String entryId) {
+    public boolean pauseDownload(String assetId) {
         // TODO: 29/01/2018 DTG pause item.
         return false;
     }
 
     @Override
-    public boolean removeEntry(String entryId) {
+    public boolean removeEntry(String assetId) {
         // TODO: 29/01/2018 DTG remove item. LAM unregister.
         return false;
     }
 
     @Override
-    public boolean checkDrmLicense(String entryId) {
+    public AssetDrmInfo getDrmStatus(String assetId) {
         // TODO: 29/01/2018 LAM check status.
-        return false;
+        return null;
     }
 
     @Override
-    public boolean renewDrmLicense(String entryId) {
+    public boolean renewAssetDrmLicense(String assetId) {
         // TODO: 29/01/2018 LAM renew.
         return false;
     }
