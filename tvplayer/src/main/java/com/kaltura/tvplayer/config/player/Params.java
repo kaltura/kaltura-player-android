24b81937348aab827c361c98b9c1d28b5fb7b746
Gilad Nadav <gilad.nadav@kaltura.com>
2019-06-20 14:38:35 +0300

    prepare to parsing json of service/Configurations/action/serveByDevice
     Gson gson = new Gson();
            PlaykitConfigurationService playkitConfigurationService = gson.fromJson(json, PlaykitConfigurationService.class);
            if (playkitConfigurationService.getParams() != null && playkitConfigurationService.getParams().getKalturaPlayer() != null) {
                log.d("partner = " + playkitConfigurationService.getParams().getKalturaPlayer().getUiConfId());
            }

---
+++ tvplayer/src/main/java/com/kaltura/tvplayer/config/player/Params.java
@@ -0,0 +1,13 @@
+package com.kaltura.tvplayer.config.player;
+
+public class Params {
+    private com.kaltura.tvplayer.config.player.KalturaPlayer KalturaPlayer;
+
+    public Params(com.kaltura.tvplayer.config.player.KalturaPlayer KalturaPlayer) {
+        this.KalturaPlayer = KalturaPlayer;
+    }
+
+    public com.kaltura.tvplayer.config.player.KalturaPlayer getKalturaPlayer() {
+        return KalturaPlayer;
+    }
+}
---
+++ tvplayer/src/main/java/com/kaltura/tvplayer/config/player/PlaykitConfigurationService.java
@@ -0,0 +1,13 @@
+package com.kaltura.tvplayer.config.player;
+
+public class PlaykitConfigurationService {
+    private Params params;
+
+    public PlaykitConfigurationService(Params params) {
+        this.params = params;
+    }
+
+    public Params getParams() {
+        return params;
+    }
+}
