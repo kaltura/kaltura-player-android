983da1ffb35a24552913184fc182f10aca4b38bf
Noam Tamim <noam.tamim@kaltura.com>
2019-07-31 23:16:03 +0300

    OVPMediaOptions: constructor with entryId

--- tvplayer/src/main/java/com/kaltura/tvplayer/OVPMediaOptions.java
+++ tvplayer/src/main/java/com/kaltura/tvplayer/OVPMediaOptions.java
@@ -7,6 +7,12 @@ public class OVPMediaOptions extends MediaOptions {
     public String entryId;
     public boolean useApiCaptions;
 
+    public OVPMediaOptions(String entryId) {
+        this.entryId = entryId;
+    }
+
+    public OVPMediaOptions() {}
+
     @Override
     public MediaEntryProvider buildMediaProvider(String serverUrl, int partnerId, String ks, String referrer) {
         return new KalturaOvpMediaProvider(serverUrl, partnerId, ks)
