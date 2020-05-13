3a836a71a2fe60925d096c8c1cee600fa8bc5701
Gilad Nadav <gilad.nadav@kaltura.com>
2020-05-13 13:58:05 +0300

    FEC-10051 fix playlist go into infinite loop in case of first media error got t… (#79)
    * fix playlist go into infinite loop in case of first media error got to next and then back again
    
    * add null protection

--- tvplayer/src/main/java/com/kaltura/tvplayer/PKPlaylistController.java
+++ tvplayer/src/main/java/com/kaltura/tvplayer/PKPlaylistController.java
@@ -168,7 +168,7 @@ public class PKPlaylistController implements PlaylistController {
         }
         String mediaId = getCacheMediaId(CacheMediaType.Current);
 
-        if (loadedMediasMap.containsKey(mediaId)) {
+        if (loadedMediasMap.containsKey(mediaId) && loadedMediasMap.get(mediaId) != null) {
             kalturaPlayer.setMedia(loadedMediasMap.get(mediaId));
             return;
         }
@@ -186,6 +186,7 @@ public class PKPlaylistController implements PlaylistController {
     private void playItemOVP(int index) {
 
         OVPMediaOptions ovpMediaOptions;
+        PKPlaylistMedia pkPlaylistMedia = playlist.getMediaList().get(index);
         if (playlistOptions instanceof OVPPlaylistOptions) {
             OVPPlaylistOptions ovpPlaylistOptions = (OVPPlaylistOptions) playlistOptions;
             ovpMediaOptions = getNextMediaOptions(index, ovpPlaylistOptions);
@@ -203,9 +204,12 @@ public class PKPlaylistController implements PlaylistController {
             }
         } else { // PlaylistId case
             OVPPlaylistIdOptions ovpPlaylistIdOptions = (OVPPlaylistIdOptions) playlistOptions;
+            if (pkPlaylistMedia == null) {
+                return; // error cannot play item
+            }
             OVPMediaAsset ovpMediaAsset = new OVPMediaAsset();
-            ovpMediaAsset.setEntryId(playlist.getMediaList().get(index).getId());
-            ovpMediaAsset.setKs((playlist.getMediaList().get(index).getKs() != null) ? playlist.getMediaList().get(index).getKs() : playlist.getKs());
+            ovpMediaAsset.setEntryId(pkPlaylistMedia.getId());
+            ovpMediaAsset.setKs((pkPlaylistMedia.getKs() != null) ? pkPlaylistMedia.getKs() : playlist.getKs());
             ovpMediaAsset.setReferrer(kalturaPlayer.getInitOptions().referrer);
 
             ovpMediaOptions = new OVPMediaOptions(ovpMediaAsset);
@@ -220,7 +224,7 @@ public class PKPlaylistController implements PlaylistController {
                 }
                 kalturaPlayer.getMessageBus().post(new PlaylistEvent.PlaylistLoadMediaError(index, new ErrorElement(loadError.getMessage(), loadError.getCode())));
             } else {
-                if (playlist.getMediaList() != null && !playlist.getMediaList().isEmpty() && playlist.getMediaList().get(index) != null) {
+                if (playlist.getMediaList() != null && !playlist.getMediaList().isEmpty() && pkPlaylistMedia != null) {
                     loadedMediasMap.put(getCacheMediaId(CacheMediaType.Current), entry);
                     log.d("OVPMedia onEntryLoadComplete entry = " + entry.getId());
                 } else {
@@ -623,10 +627,15 @@ public class PKPlaylistController implements PlaylistController {
         } else if (cacheMediaType == CacheMediaType.Prev) {
             mediaListIndex -= 1;
         }
+        
+        PKPlaylistMedia pkPlaylistMedia = playlist.getMediaList().get(mediaListIndex);
+        if (pkPlaylistMedia == null) {
+            return "";
+        }
 
-        String mediaId = playlist.getMediaList().get(mediaListIndex).getId();
+        String mediaId = pkPlaylistMedia.getId();
         if (kalturaPlayer.getTvPlayerType() == KalturaPlayer.Type.ott) {
-            mediaId = playlist.getMediaList().get(mediaListIndex).getMetadata().get("entryId");
+            mediaId = pkPlaylistMedia.getMetadata().get("entryId");
         }
         return mediaId;
     }
