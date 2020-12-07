69a6630c5f686acf5d36b15d61a9556264de5e4b
Gilad Nadav <gilad.nadav@kaltura.com>
2020-12-07 10:32:35 +0200

    Update OVPMediaOptions.java

--- tvplayer/src/main/java/com/kaltura/tvplayer/OVPMediaOptions.java
+++ tvplayer/src/main/java/com/kaltura/tvplayer/OVPMediaOptions.java
@@ -56,3 +56,5 @@ public class OVPMediaOptions extends MediaOptions {
                 .setEntryId(ovpMediaAsset.getEntryId()).setReferenceId(ovpMediaAsset.getReferenceId()).setUseApiCaptions(this.useApiCaptions).setReferrer(ovpMediaAsset.getReferrer());
     }
 }
+
+
