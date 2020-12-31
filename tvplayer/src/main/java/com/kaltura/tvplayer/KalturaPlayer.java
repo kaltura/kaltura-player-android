e3ede1f057c310417022d9c50218720eb47805b1
Gilad Nadav <gilad.nadav@kaltura.com>
2020-12-31 10:13:11 +0200

    FEC-10843 - expose seekToLiveDefaultPosition API (#107)
    * expose seekToLiveDefaultPosition API
    
        public void seekToLiveDefaultPosition() {
            pkPlayer.seekToLiveDefaultPosition();
        }

--- tvplayer/src/main/java/com/kaltura/tvplayer/KalturaPlayer.java
+++ tvplayer/src/main/java/com/kaltura/tvplayer/KalturaPlayer.java
@@ -631,6 +631,10 @@ public abstract class KalturaPlayer {
         pkPlayer.seekTo(position);
     }
 
+    public void seekToLiveDefaultPosition() {
+        pkPlayer.seekToLiveDefaultPosition();
+    }
+
     public AdController getAdController() {
         return pkPlayer.getController(AdController.class);
     }
