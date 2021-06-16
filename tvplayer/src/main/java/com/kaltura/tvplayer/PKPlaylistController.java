ab43dae2837640a388345b5f9255ae6b65c70088
Gourav Saxena <gourav.saxena@kaltura.com>
2021-06-16 16:04:06 +0530

    - Travis build

--- tvplayer/src/main/java/com/kaltura/tvplayer/PKPlaylistController.java
+++ tvplayer/src/main/java/com/kaltura/tvplayer/PKPlaylistController.java
@@ -645,7 +645,6 @@ public class PKPlaylistController implements PlaylistController {
                 kalturaPlayer.getCurrentPosition() > 0 && kalturaPlayer.getDuration() > 0 && kalturaPlayer.getCurrentPosition() >= kalturaPlayer.getDuration();
     }
 
-    
     private String getCacheMediaId(CacheMediaType cacheMediaType) {
 
         if (currentPlayingIndex < 0 || currentPlayingIndex >= playlist.getMediaList().size()) {
