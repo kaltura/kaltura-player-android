b4dbdf1adec1279e659728bd4a55bb7bc1188a32
Gilad Nadav <gilad.nadav@kaltura.com>
2021-07-26 12:39:14 +0300

    cleanup

--- tvplayer/src/main/java/com/kaltura/tvplayer/offline/exo/PrefetchConfig.java
+++ tvplayer/src/main/java/com/kaltura/tvplayer/offline/exo/PrefetchConfig.java
@@ -3,21 +3,14 @@ package com.kaltura.tvplayer.offline.exo;
 public class PrefetchConfig {
 
     int maxItemCountInCache = 20;
-//    int maxCacheSize = 100; // in MB
     int assetPrefetchSize = 2; // in MB
     boolean removeCacheOnDestroy = true;
-//    boolean freeCacheIfFull = true; // remove first one by time stamp?
 
     public PrefetchConfig setMaxItemCountInCache(int maxItemCountInCache) {
         this.maxItemCountInCache = maxItemCountInCache;
         return this;
     }
 
-//    public PrefetchConfig setMaxCacheSize(int maxCacheSize) {
-//        this.maxCacheSize = maxCacheSize;
-//        return this;
-//    }
-//
     public PrefetchConfig setAssetPrefetchSize(int assetPrefetchSize) {
         this.assetPrefetchSize = assetPrefetchSize;
         return this;
@@ -28,19 +21,10 @@ public class PrefetchConfig {
         return this;
     }
 
-//    public PrefetchConfig setFreeCacheIfFull(boolean freeCacheIfFull) {
-//        this.freeCacheIfFull = freeCacheIfFull;
-//        return this;
-//    }
-
     public int getMaxItemCountInCache() {
         return maxItemCountInCache;
     }
 
-//    public int getMaxCacheSize() {
-//        return maxCacheSize;
-//    }
-//
     public int getAssetPrefetchSize() {
         return assetPrefetchSize;
     }
@@ -48,8 +32,4 @@ public class PrefetchConfig {
     public boolean isRemoveCacheOnDestroy() {
         return removeCacheOnDestroy;
     }
-
-//    public boolean freeCacheIfFull() {
-//        return freeCacheIfFull;
-//    }
 }
